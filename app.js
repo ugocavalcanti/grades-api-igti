82addf6d89df370495c95c6af4d201c65d14e884
Ugo Cavalcanti <ugocavalcanti@gmail.com>
2020-07-09 12:14:51 -0300

    correcao de url no cors

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:3000/',
-    // origin: 'https://stormy-scrubland-34583.herokuapp.com/',
+    // origin: 'http://localhost:3000',
+    origin: 'https://stormy-scrubland-34583.herokuapp.com',
   })
 );
 
